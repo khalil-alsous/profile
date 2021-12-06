867df4be994aea9a1516e91bfb352f039f1e6611
khalil Alsous <khalilalsous2020@gmail.com>
2021-12-06 23:19:11 +0200

    edit1

--- src/main.js
+++ src/main.js
@@ -12,8 +12,18 @@ import "./scss/main.scss";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+library.add(faFacebook);
 library.add(faTwitter);
+library.add(faGoogle);
+library.add(faInstagram);
+library.add(faLinkedin);
+library.add(faGithub);
 
 
 createApp(App)
